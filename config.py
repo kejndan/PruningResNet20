8efb22b623c643548bf1c2e9514a895951c4338d
Adel <kennedy.presentxxi@gmail.com>
2021-03-13 12:30:44 +0300

    add config

---
+++ config.py
@@ -0,0 +1,41 @@
+from easydict import EasyDict
+
+cfg = EasyDict()
+cfg.random_seed = 0
+cfg.device = 'cuda:0'
+
+cfg.path_to_dataset = 'C:\\Users\\adels\PycharmProjects\datasets\cifar10'
+cfg.path_to_saves = 'saves'
+cfg.path_to_logs = 'logs'
+
+cfg.clearml_logging = False
+cfg.project_name = 'Demo'
+cfg.task_name = 'Best score'
+
+cfg.evaluate_on_train_data = True
+cfg.evaluate_on_validation_data = False
+cfg.valid_size = 0.2
+
+cfg.load_save = False
+cfg.name_save = 'best_checkpoint'
+
+
+cfg.batch_size = 128
+cfg.lr = 1e-1
+cfg.total_epochs = 100
+
+
+cfg.save_models = True
+cfg.save_best_model = True
+
+
+cfg.use_lr_scheduler = True
+cfg.ROP_patience = 5
+cfg.ROP_factor = 0.1
+
+cfg.plot_confusion_matrix = True
+
+
+
+
+
